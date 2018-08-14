0788e33856da3494e8403b329040f99c80697e73
Cole Ramos <cole_ramos@hotmail.com>
2018-08-14 13:38:52 -0700

    Add files via upload
    Added an updated login script

--- instagram_login.py
+++ instagram_login.py
@@ -1,14 +1,22 @@
-#The initial login?
-payload = {
-    "username": "ENTER_USERNAME_HERE",
-    "password": "ENTER_PASSWORD_HERE",
-    "csrf_token": "N5jvKWRKxWCUt2JZMo4dsKtjFgXqYX0a"
-}
+import requests
+import json
 
-from lxml import html
+BASE_URL = 'https://www.instagram.com'
+LOGGIN_URL = BASE_URL + 'accounts/login/?hl=en'
+USERNAME = 'coleramos425'
+PASSWD = 'Bellevue97'
+USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36'
 
-session_requests = requests.session()
-login_url = 'https://www.instagram.com/accounts/login/?hl=en'
-result = session_requests.get(login_url)
+session = requests.Session()
+session.headers = {'user-agent' : USER_AGENT}
+session.headers.update({'Referer' : BASE_URL})
 
-tree = html.frontstring(result.text)
+req = session.get(BASE_URL)
+session.headers.update({'X-CSRFToken' : req.cookies['csrftoken']})
+login_data = {'username' : USERNAME, 'password' : PASSWD}
+login = session.post(LOGGIN_URL, data = login_data, allow_redirects=True)
+session.headers.update({'X-CSRFToken' : login.cookies['csrftoken']})
+cookies = login.cookies
+login_text = json.loads(login.text)
+
+print(login_text)
